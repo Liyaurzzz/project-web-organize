44b5847f435e8551f7a61d32d6ae4946352101bb
Lyy <syaidinnaalli05@gmail.com>
2026-09-17 14:42:49 +0700

    Add files via upload

---
+++ project-web-organize/config/connection.php
@@ -0,0 +1,7 @@
+<?php
+
+$namaVariabel = "hello brew";
+echo "<h1>". $namaVariabel . "</h1>";
+
+$namaSaya = "Lyy";
+?>
---
+++ project-web-organize/index.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once ('config/connection.php');
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1> hey dear </h1>
+    <script src="assets/js/hello.js"></script>
+    <?php echo "<p>". $namaSaya . "</p>" ?>
+
+    <img src="images.jpg" width="250">
+</body>
+</html>
